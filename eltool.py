f055f6f883f87430156c3952625d6586b9e8b746
Stephen Bryant <sbryant@xvt.com>
2015-11-07 00:10:37 -0500

    Added total

--- eltool.py
+++ eltool.py
@@ -58,7 +58,37 @@ class ELTool:
 		self.start_button.grid( column=5, row=0, rowspan=2 )
 		self.test_button.grid( column=5, row=2, rowspan=2 )
 
+		self.total_win = TotalWin( self.root )
+		self.total_win.wm_title( "ELTool Total" )
+		self.total_win.grid_columnconfigure( 0, weight=1 )
+		self.total_win.grid_rowconfigure( 0, weight=1 )
+
+		self.update_total()
 		self.root.mainloop()
+
+	def get_total( self ):
+		self.partID = self.player_id_entry.get()
+		r = requests.get( "http://www.extra-life.org/index.cfm?fuseaction=widgets.ajaxWidgetCompileHTML&language=en&participantid0={}&eventid0=525&orientation0=horizontal&currencyformat0=none&type0=thermometer&participantid1={}&eventid1=525".format( self.partID, self.partID ) )
+
+		try:
+			json = r.json()
+			bs = BeautifulSoup( json['values']['renderedWidgets'], "html.parser" )
+		except:
+			return None
+
+		raised = bs.find( class_ = "dd-thermo-raised" ).contents[0]
+		goal = bs.find( class_ = "dd-thermo-goal" ).contents[2].strip()
+
+		return "{} / {}".format( raised, goal )
+
+	def update_total( self ):
+		try:
+			total = self.get_total()
+			self.total_win.set_total( total )
+		except:
+			pass
+
+		self.root.after( 30000, self.update_total )
 
 	def check_donations( self ):
 		self.partID = self.player_id_entry.get()
@@ -93,12 +123,20 @@ class ELTool:
 		self.player_win.wm_title( "ELTool Display" )
 		self.player_win.grid_columnconfigure( 0, weight=1 )
 		self.player_win.grid_rowconfigure( 0, weight=1 )
+		self.player_win.protocol( "WM_DELETE_WINDOW", self.close_player_win )
 
 		self.player = GIFPlayer( self.player_win, self.gif_file )
 		self.player.set_background_color( self.back_color )
 		self.player.set_text_color( self.text_color )
 
+	def close_player_win( self ):
+		self.player_win.destroy()
+		self.player_win = None
+
 	def start( self ):
+		if not self.player_win:
+			self.create_player_win()
+
 		if self.is_started:
 			self.is_started = False
 			self.start_button.config( text="Start" )
@@ -110,9 +148,11 @@ class ELTool:
 			self.root.after( 1000, self.check_queue )
 
 	def play_anim( self, text ):
-		if self.player:
-			self.player.set_font( self.font_name_entry.get(), self.font_size_entry.get() )
-			self.player.play( text, self.check_queue )
+		if not self.player_win:
+			self.create_player_win()
+
+		self.player.set_font( self.font_name_entry.get(), self.font_size_entry.get() )
+		self.player.play( text, self.check_queue )
 
 		if winsound and self.wav_file:
 			winsound.PlaySound( self.wav_file, winsound.SND_ASYNC )
@@ -122,22 +162,20 @@ class ELTool:
 			return
 
 		if len( self.donationsQueue ) > 0:
-			self.play_anim( self.donationsQueue.pop(0) )
+			try: # for some reason this occasionally fails, and I don't have the time to debug it.
+				self.play_anim( self.donationsQueue.pop(0) )
+			except:
+				self.root.after( 1000, self.check_queue )
 		else:
 			self.root.after( 1000, self.check_queue )
 
 	def pick_gif( self ):
-		self.gif_file = filedialog.askopenfilename( filetypes=[("GIF", "*.gif"), ], initialdir="." )
-		if self.gif_file:
+		gif = filedialog.askopenfilename( filetypes=[("GIF", "*.gif"), ], initialdir="." )
+		if gif:
+			self.gif_file = gif
 			self.start_button.config( state=tkinter.NORMAL )
 			self.test_button.config( state=tkinter.NORMAL )
 			self.create_player_win()
-		else:
-			self.start_button.config( state=tkinter.DISABLED )
-			self.test_button.config( state=tkinter.DISABLED )
-
-			if self.is_started:
-				self.start() # Should toggle to stopped
 
 	def pick_wav( self ):
 		self.wav_file = filedialog.askopenfilename( filetypes=[("WAV", "*.wav"), ], initialdir="." )
@@ -151,5 +189,25 @@ class ELTool:
 		self.text_color = colorchooser.askcolor()[1]
 		if self.player:
 			self.player.set_text_color( self.text_color )
+
+
+class TotalWin( tkinter.Toplevel ):
+
+	def __init__( self, parent ):
+		width = 400
+		height = 200
+
+		tkinter.Toplevel.__init__( self, parent, width=width, height=height, background='green' )
+
+		self.geometry( "{}x{}".format( width, height ) )
+
+		self.canvas = tkinter.Canvas( self, background='green' )
+		self.canvas.grid( row=0, column=0, sticky=tkinter.NSEW )
+
+		self.text_id = self.canvas.create_text( ( width/2, height/2 ), font=( "Arial", 40 ), fill='#ffffff', width=width, justify=tkinter.CENTER )
+
+	def set_total( self, total ):
+		self.canvas.itemconfig( self.text_id, text=total )
+
 
 tool = ELTool()
